9ca21596d4b3f7bacc9fe38420b50a98f4121319
cusas <cusas@hotmail.com>
2021-02-02 15:05:40 +0800

    init

--- README.md
+++ README.md
@@ -1,6 +1,40 @@
-## APPPullerAndDec
+## AndroidDiggerOne
 从手机中拖出所有APP，并反编译成Java源码，最后利用opengrok创建索引
 
 ### 环境准备
 1、安装adb
 sudo apt install adb
+
+2、获得vdexExtrator
+git clone https://github.com/anestisb/vdexExtractor
+cd vdexExtractor
+./make.sh
+./make.sh gcc
+vim ~/.bashrc
+Append a line==>export PATH=$PATH:~/tools/vdexExtractor/bin
+
+3、获得compact_dex_converters
+download it from: https://github.com/anestisb/vdexExtractor/issues/23
+compact_dex_converter_linux.zip
+Extrat to AndroidDiggerOne/bin/
+
+4、获得jadx
+sudo apt-get install openjdk-8-jdk
+Download from: https://github.com/skylot/jadx/releases/tag/v1.2.0
+vim ~/.bashrc
+Append a line==>export PATH=$PATH:~/tools/vdexExtractor/bin:~/tools/jadx-1.2.0/bin
+
+5、安装apache-tomcat
+sudo apt-get install default-jdk
+sudo apt-get install tomcat8
+service tomcat8 status
+
+6、获得opengrok
+sudo apt-get install exuberant-ctags
+https://github.com/oracle/opengrok/wiki/How-to-setup-OpenGrok
+sudo cp ~/tools/opengrok-1.5.11/lib/source.war /var/lib/tomcat8/webapps/
+
+### 运行工具
+python2 ./pull_dec.py
+
+
--- puller.py
+++ puller.py
@@ -3,7 +3,7 @@
 
 import os
 
-basePath="./data/apk"
+basePath="./data/apk/" #nust be end with /
 
 def getrightstr(pstr,psub):
     ipos=pstr.find(psub)
@@ -32,7 +32,16 @@ def getleftstrrev(pstr,psub):
 def printhex(pstr):
     for char in pstr:
         print ord(char)
-        
+fo=open("exclude","r")
+exclude_apps=[]
+for line in fo.readlines():
+    exclude_apps.append(line.strip())
+def ignore_app(appName):
+    global exclude_apps
+    for app in exclude_apps:
+        if app == appName:
+            return True
+    return False
 # step1: pull all apps
 os.popen('adb root')
 packlist = os.popen('adb shell "pm list packages"')
@@ -44,11 +53,14 @@ while True:
         break
     packname=line.replace('\n','').replace('\r','').replace('package:','')
     print '['+packname+']'
+    if ignore_app(packname)==True:
+        continue
     line='adb shell pm path '+packname
     print 'cmd==>'+line
     path = os.popen(line)
     line=path.read().replace('\n','').replace('\r','')
     print 'ret==>'+line
+    split_count=0
     while line.find('package:')>=0:
         apppath=getrightstr(line,'package:')
         apppath=getleftstr(apppath,'package:')
@@ -56,7 +68,11 @@ while True:
         # print 'apppath='+apppath
         # print 'line='+line
         if apppath.find("/data/app/")>=0:
-            pullcmd='adb pull '+apppath+' '+basePath+'/'+packname+'.apk'
+            if split_count==0:
+                pullcmd='adb pull '+apppath+' '+basePath+'/'+packname+'.apk'
+            else:
+                pullcmd='adb pull '+apppath+' '+basePath+'/'+packname+'_'+str(split_count)+'.apk'
+            split_count+=1
         else:
             pullcmd='adb pull '+getleftstrrev(apppath,"/")+'/ '+basePath+'/'
         print 'cmd==>'+pullcmd
@@ -65,9 +81,9 @@ while True:
         print 'ret==>'+pullresult
         if pullresult.find('100%')<0:
             fail_list+='['+packname+']'+pullcmd+'\n'
-    count+=1
-    if count>10:
-        break
+    #count+=1
+    #if count>10:
+    #    break
 print '[Fail list==============>\n'+fail_list
 
 # step2: decompile vdex->cdex->dex
@@ -83,7 +99,7 @@ def decompilePath(curPath,depth):
             filetype=getrightstrrev(file,".")
             if filetype=="vdex":
                 print(curPath+file)
-                line="./bin/vdexExtractor -i "+curPath+file
+                line="vdexExtractor -i "+curPath+file
                 pull=os.popen(line)
                 print(pull.read())
         else:
@@ -100,7 +116,7 @@ def decompilePathCdex(curPath,depth):
             filetype=getrightstrrev(file,".")
             if filetype=="cdex":
                 print(curPath+file)
-                line="./bin/compact_dex_converters "+curPath+file
+                line="./bin/compact_dex_converter "+curPath+file
                 pull=os.popen(line)
                 print(pull.read())
         else:
@@ -108,3 +124,52 @@ def decompilePathCdex(curPath,depth):
 
 decompilePath(basePath,1)
 decompilePathCdex(basePath,1)
+
+# step3: jadx decompile
+startflag=True
+def decompilePath(curPath,count):
+    global startflag
+    if count>30:
+        print (curPath)
+        return
+    dirs=os.listdir(curPath)
+    for file in dirs:
+        if file=="" or file=="." or file=="..":
+            count=count
+        elif os.path.isfile(curPath+file):
+            if startflag==False:
+                if file=="com.xx.smarthome.apk":
+                    startflag=True
+            else:
+                filetype=getrightstrrev(file,".")
+                if filetype=="apk":
+                    print(curPath+file)
+                    sfile=curPath+file
+                    dfile=sfile.replace("/apk/","/java/")
+                    dfile=dfile.replace("/oat/arm64/","/")
+                    line="jadx --show-bad-code --deobf --export-gradle --escape-unicode -j 4 -d "+dfile+" "+sfile
+                    pull=os.popen(line)
+                    print(pull.read())
+                if filetype=="new":
+                    print(curPath+file)
+                    sfile=curPath+file
+                    sfile=sfile.replace(".new",".dex")
+                    os.rename(curPath+file,sfile)
+                    dfile=curPath.replace("/apk/","/java/")
+                    dfile=dfile.replace("/oat/arm64/","/")
+                    line="jadx --show-bad-code --deobf --export-gradle --escape-unicode -j 4 -d "+dfile+" "+sfile
+                    pull=os.popen(line)
+                    print(pull.read())
+                if filetype=="dex":
+                    print(curPath+file)
+                    sfile=curPath+file
+                    dfile=curPath.replace("/apk/","/java/")
+                    dfile=dfile.replace("/oat/arm64/","/")
+                    line="jadx --show-bad-code --deobf --export-gradle --escape-unicode -j 4 -d "+dfile+" "+sfile
+                    pull=os.popen(line)
+                    print(pull.read())
+        else:
+            decompilePath(curPath+file+"/",count+1)
+            
+decompilePath(basePath,1)
+
